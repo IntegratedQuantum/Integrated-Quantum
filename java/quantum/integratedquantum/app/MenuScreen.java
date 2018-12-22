d2348d737c98d091ebdb4105b3e2e6034fb6c22d
IntegratedQuantum <43880493+IntegratedQuantum@users.noreply.github.com>
2018-12-22 15:34:01 +0100

    Include text to the app

--- java/quantum/integratedquantum/app/MenuScreen.java
+++ java/quantum/integratedquantum/app/MenuScreen.java
@@ -24,20 +24,23 @@ class MenuScreen extends Screen implements ActionListener {
             {"Introduction", "Black Body Radiation", "Interference", "Wave Function", "Superposition", "Wave Particle Dualism I", "Wave Particle Dualism II", "Tunnel Effect", "Uncertainty Principle", "Particles", "Light", "Pauli Exclusion", "Spin", "Vacuum Fluctuation", "Quantum Entanglement", "Interpretations"},
             {"Notice", "Differentiation", "Integration", "Imaginary Numbers", "Complex Numbers", "Differential Equations", "Partial Differential Equations"},
             {"Schrödinger Equation", "Basic Calculations I", "Quantum Well", "Quantum Well II", "Basic Calculations II", "Basic Calculations III", "Operators", "Superposition", "Wave Particle Dualism II", "Tunnel Effect", "Uncertainty Principle", "Electronvolt", "Pauli Exclusion", "Spin"},
+            {"Numerical Approach I"},
             {"Settings"},
         };
         subName = new String[][]{
             {"", "", "", "", "", "Double Slit Experiment", "Photoelectric Effect", "", "", "", "", "", "", "", "", ""},
             {"", "", "", "", "", "", ""},
             {"", "constant Potential", "", "Finite Potential Well", "non-radial symmetric Potential", "radial symmetric Potential", "", "", "Photoelectric Effect", "", "", "", "", ""},
+            {"1D Symmetric Potential"},
             {" "},
         };
-        String [] name2 = {"Principles of Quantum Physics", "Mathematical Foundation", "Quantum Principle Calculations", "Extras"};
-        Panel[] ai = new Panel[4];
+        String [] name2 = {"Principles of Quantum Physics", "Mathematical Foundation", "Quantum Principle Calculations", "Numerical Solution", "Extras"};
+        Panel[] ai = new Panel[5];
         ai[0] = new Panel(0, 1790, 0, 180, 540, 830, Assets.bgP);
         ai[1] = new Panel(0, 350, 0, 180, 540, 830, Assets.bgP);
-        ai[2] = new Panel(0, 1310, 0, 180, 540, 830, Assets.bgP);
+        ai[2] = new Panel(0, 1470, 0, 180, 540, 830, Assets.bgP);
         ai[3] = new Panel(0, 0, 0, 180, 540, 830, Assets.bgP);
+        ai[4] = new Panel(0, 0, 0, 180, 540, 830, Assets.bgP);
         for(int j = 0; j < ai.length; j++) {
             for(int i = 0; i < name[j].length; i++) {
                 Button a = new Button(10, 10 + 160 * i, 520, 150, name[j][i], this);
